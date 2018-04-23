bc19d9748195e46a9850ea3c901749dbca75a046
chuyou <chuyou@pinduoduo.com>
2018-04-23 14:09:29 +0800

    add environment

--- flink-docker/docker-entrypoint.sh
+++ flink-docker/docker-entrypoint.sh
@@ -28,13 +28,15 @@ if [ "$1" == "--help" -o "$1" == "-h" ]; then
 elif [ "$1" == "jobmanager" ]; then
     echo "Starting Job Manager"
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" $FLINK_HOME/conf/flink-conf.yaml
+    sed -i -e "s/jobmanager.heap.mb: 1024/jobmanager.heap.mb: ${JOB_MANAGER_HEAP_MB}/g" $FLINK_HOME/conf/flink-conf.yaml
 
     echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
     exec $FLINK_HOME/bin/jobmanager.sh start-foreground cluster
 elif [ "$1" == "taskmanager" ]; then
 
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" $FLINK_HOME/conf/flink-conf.yaml
-    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $(grep -c ^processor /proc/cpuinfo)/g" $FLINK_HOME/conf/flink-conf.yaml
+    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: ${NUMBER_OF_TASK_SLOTS}/g" $FLINK_HOME/conf/flink-conf.yaml
+    sed -i -e "s/taskmanager.heap.mb: 1024/taskmanager.heap.mb: {TASK_MANAGER_HEAP_MB}/g" $FLINK_HOME/conf/flink-conf.yaml
 
     echo "Starting Task Manager"
     echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
